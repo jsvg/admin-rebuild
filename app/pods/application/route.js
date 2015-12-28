fc9c27d81f9ae4f51873f92a6066cc7452bd9fdb
jsvg <vangiessen_julian@bah.com>
2015-12-27 21:35:29 -0500

    decomposed large css file to manageable scss files

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 const { $, on, Route } = Ember;
 export default Route.extend({
   jqSetup: on('activate', function() {
-    // adds theme skins to page
-    $('body').addClass('theme sidebar-collapse sidebar-mini');
+    // prep sidebar
+    $('body').addClass('sidebar-collapse sidebar-mini');
     // fix for IE page transitions
     $("body").removeClass("hold-transition");
   })
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,21 +8,22 @@ module.exports = function(defaults) {
       useScss: true
     }
   });
+  
+  app.import('bower_components/Ionicons/css/ionicons.css');
+  app.import('bower_components/Ionicons/fonts/ionicons.eot', {destDir: "fonts"});
+  app.import('bower_components/Ionicons/fonts/ionicons.woff', {destDir: "fonts"});
+  app.import('bower_components/Ionicons/fonts/ionicons.svg', {destDir: "fonts"});
+  app.import('bower_components/Ionicons/fonts/ionicons.ttf', {destDir: "fonts"});
 
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.eot', {destDir: "fonts"});
   app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.svg', {destDir: "fonts"});
   app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.ttf', {destDir: "fonts"});
   app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff', {destDir: "fonts"});
   app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff2', {destDir: "fonts"});
 
-  // plugins need to be imported
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import('bower_components/interact/interact.js');
   app.import('bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js');
-
-  app.import('vendor/custom-theme.css');
 
   return app.toTree();
 };
