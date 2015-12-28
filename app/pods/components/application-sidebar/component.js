809bb5deded7ad0432640127c77b21ef5ee3d862
jsvg <vangiessen_julian@bah.com>
2015-12-28 02:19:28 -0500

    tailoring styles a bit

--- app/pods/components/application-sidebar/component.js
+++ app/pods/components/application-sidebar/component.js
@@ -1,9 +1,11 @@
 // C application-header
 import Ember from 'ember';
+
 const { Component } = Ember;
 export default Component.extend({
   tagName: 'aside',
   classNames: ['main-sidebar'],
+
   didInsertElement() {
     this.$().css('position','fixed');
   }
